bb12a26cdd88cb8a9bbdf32bc5b05bb3b9ff7e1d
Greg Marineau <greg.marineau@devfactory.ch>
2014-11-11 17:55:29 +0100

    Update the provider file with the good path

--- src/ElshopServiceProvider.php
+++ src/ElshopServiceProvider.php
@@ -5,43 +5,42 @@ use Illuminate\Support\Facades\View;
 
 class ElshopServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+  /**
+   * Indicates if loading of the provider is deferred.
+   *
+   * @var bool
+   */
+  protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('devfactory/elshop', NULL, __DIR__);
-		View::addNamespace('devfactory/elshop', __DIR__ . '/../../views');
-		include __DIR__ . '/../../routes.php';
-	}
+  /**
+   * Bootstrap the application events.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+    $this->package('devfactory/elshop', 'elshop', __DIR__);
+    include __DIR__ . 'routes.php';
+  }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		//
-	}
+  /**
+   * Register the service provider.
+   *
+   * @return void
+   */
+  public function register()
+  {
+    //
+  }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
+  /**
+   * Get the services provided by the provider.
+   *
+   * @return array
+   */
+  public function provides()
+  {
+    return array();
+  }
 
 }
