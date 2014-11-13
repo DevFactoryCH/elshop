df039b6853a6e42d8207d1514fb52d32ece69e58
Greg Marineau <greg.marineau@devfactory.ch>
2014-11-13 17:08:48 +0100

    Added the english language file

---
+++ src/lang/en/article.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+  'name' => 'Name',
+  'teaser' => 'Teaser',
+  'description' => 'Description',
+  'price' => 'Price',
+  'sale_price' => 'Sale price',
+  'brand' => 'Brand',
+  'weight' => 'Weight',
+  'ean13' => 'EAN13',
+);
--- src/views/articles/create.blade.php
+++ src/views/articles/create.blade.php
@@ -7,38 +7,38 @@
       {{ Form::open(array('route' => $prefix . 'articles.store')) }}
         <div class="row">
           <div class="form-group col-sm-10">
-            {{ Form::label('name', trans('article.name')) }}
+            {{ Form::label('name', trans('elshop::article.name')) }}
             {{ Form::text('name', Input::get('name'), array('class' => 'form-control')) }}
             {{ $errors->first('name', '<span class="text-danger">:message</span>') }}
           </div>
           <div class="form-group col-sm-2">
-            {{ Form::label('brand', trans('article.brand')) }}
+            {{ Form::label('brand', trans('elshop::article.brand')) }}
             {{ Form::select('brand', $brands, Input::get('brand'), array('class' => 'form-control')) }}
           </div>
         </div>
         <div class="form-group">
-          {{ Form::label('teaser', trans('article.teaser')) }}
+          {{ Form::label('teaser', trans('elshop::article.teaser')) }}
           {{ Form::textarea('teaser', Input::get('teaser'), array('class' => 'form-control', 'rows' => 3)) }}
           {{ $errors->first('teaser', '<span class="text-danger">:message</span>') }}
         </div>
         <div class="form-group">
-          {{ Form::label('description', trans('article.description')) }}
+          {{ Form::label('description', trans('elshop::article.description')) }}
           {{ Form::textarea('description', Input::get('name'), array('class' => 'form-control')) }}
           {{ $errors->first('description', '<span class="text-danger">:message</span>') }}
         </div>
         <div class="row">
           <div class="form-group col-sm-3">
-            {{ Form::label('price', trans('article.price')) }}
+            {{ Form::label('price', trans('elshop::article.price')) }}
             {{ Form::text('price', Input::get('price'), array('class' => 'form-control')) }}
             {{ $errors->first('price', '<span class="text-danger">:message</span>') }}
           </div>
           <div class="form-group col-sm-3">
-            {{ Form::label('sale_price', trans('article.sale_price')) }}
+            {{ Form::label('sale_price', trans('elshop::article.sale_price')) }}
             {{ Form::text('sale_price', Input::get('sale_price'), array('class' => 'form-control')) }}
             {{ $errors->first('sale_price', '<span class="text-danger">:message</span>') }}
           </div>
           <div class="form-group col-sm-3">
-            {{ Form::label('weight', trans('article.weight')) }}
+            {{ Form::label('weight', trans('elshop::article.weight')) }}
             <div class="input-group">
               {{ Form::text('weight', Input::get('weight'), array('class' => 'form-control')) }}
               <span class="input-group-addon">KGr.</span>
@@ -46,7 +46,7 @@
             {{ $errors->first('weight', '<span class="text-danger">:message</span>') }}
           </div>
           <div class="form-group col-sm-3">
-            {{ Form::label('ean13', trans('article.ean13')) }}
+            {{ Form::label('ean13', trans('elshop::article.ean13')) }}
             {{ Form::text('ean13', Input::get('ean13'), array('class' => 'form-control')) }}
             {{ $errors->first('ean13', '<span class="text-danger">:message</span>') }}
           </div>
