94b2f8590eddfefb077eed2642491100fb5bc3b9
Greg Marineau <greg.marineau@devfactory.ch>
2015-01-12 15:14:35 +0100

    Add a new funciton to calculate the parcel price

--- src/models/Parcel.php
+++ src/models/Parcel.php
@@ -14,8 +14,28 @@ class Parcel extends Eloquent {
 
   public $timestamps = FALSE;
 
-  public static function price($weight) {
-    
+  /**
+   * Get the parcel price for an order 
+   * @param  int $total    Can be the total price of the order or the total weight
+   * @return int return False if we not found parcel price else we return the parcel price
+   */
+  public static function getPrice($total) {
+    $parcels = Parcel::all();
+    $total = $total * 100;
+
+    foreach ($parcels as $parcel) {
+      if ($parcel->type == 0 && $total >= $parcel->min && $total <= $parcel->max) {
+        return $parcel->price;
+      }
+      elseif ($total <= $parcel->min && $parcel->type == 1) {
+        return $parcel->price;
+      }
+      elseif ($total >= $parcel->min && $parcel->type == 2) {
+        return $parcel->price;
+      }
+    }
+
+    return FALSE;
   }
   
 }
