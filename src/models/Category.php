31f086044be8448e553395b3dd9d9441f1944f51
Greg Marineau <greg.marineau@devfactory.ch>
2015-05-18 15:39:37 +0200

    Added new function for categories

--- src/models/Category.php
+++ src/models/Category.php
@@ -20,5 +20,29 @@ class Category extends Eloquent {
     return $this->hasMany('Devfactory\Elshop\Models\Category');
   }
 
+  public function articles() {
+    return $this->belongsToMany('Devfactory\Elshop\Models\Article', 'articles_categories');
+  }
 
+  /**
+   * Get all children from a category
+   * 
+   * @return array array with all children id
+   */
+  public function getChildren() {
+    $categories[] = $this->id;
+    foreach ($this->categories()->get() as $category) {
+      $categories[] = $category->id;
+    }
+
+    return $categories;
+  }
+
+  public function parent() {
+    if ($this->category_id) {
+      return $this->belongsTo('Devfactory\Elshop\Models\Category', 'category_id');
+    }
+
+    return FALSE;
+  }
 }
