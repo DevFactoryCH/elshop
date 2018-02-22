23e2950d25f4d9ade0bfd0ca5b23c81eb7faa88a
Raphael Angehrn <raphael.angehrn@devfactory.ch>
2018-02-22 16:06:17 +0100

    version pre menu admin

---
+++ src/app/Models/Item.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Devfactory\Elshop\App\Models;
+
+use Zizaco\Entrust\EntrustPermission;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Item extends Model
+{
+
+
+    public $fillable = ['title','description'];
+
+
+}
